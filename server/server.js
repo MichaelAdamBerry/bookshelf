585a23cdaa9a851c0cbd8ce4dde9ae1d806f763b
MIchael Berry <michaeladamberry3@gmail.com>
2019-02-10 15:55:53 -0500

    checking in

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// require("./routes")(app);
+
+app.get("/", (req, res) => {
+  res.send("PORT 5000");
+});
+
+// create a GET route
+app.get("/express_backend", (req, res) => {
+  res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
+});
+
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));

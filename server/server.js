2736f42bb241e584042bb898bacf83a6bcde7221
MIchael Berry <michaeladamberry3@gmail.com>
2019-02-13 23:52:58 -0500

    trouble shooting

--- server/server.js
+++ server/server.js
@@ -45,14 +45,14 @@ app.get("/list-data/:query", (req, res) => {
 //for production mode
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.join((__dirname = "client/build/index.html")));
   });
 }
 
 //for build mode
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/public/index.html"));
 });
 
